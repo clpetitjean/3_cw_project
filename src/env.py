b1517b3f6fa6f920c9d99a3c1d64cd738f0ff414
ClementPetitjean <98564195+ClementPetitjean@users.noreply.github.com>
2024-08-26 01:30:07 +0200

    Add files via upload

---
+++ src/env.py
@@ -0,0 +1,7 @@
+PATH = '/Users/cleme/PycharmProjects/Coding_Weeks/Twitter_Fake_News_Detection'
+PATH_DATA = '/Users/cleme/PycharmProjects/Coding_Weeks/Twitter_Fake_News_Detection/data'
+
+PATH_DATASET_TRAIN = '/Users/cleme/PycharmProjects/Coding_Weeks/Twitter_Fake_News_Detection/data/kaggle/train.csv'
+PATH_DATA_JSON = '/Users/cleme/PycharmProjects/Coding_Weeks/Twitter_Fake_News_Detection/data/tweets_elonmusk.json'
+
+PATH_TRAINED_MODEL = '/Users/cleme/PycharmProjects/Coding_Weeks/Twitter_Fake_News_Detection/config/trained_model.joblib'
